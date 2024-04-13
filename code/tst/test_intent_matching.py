91b202f4a9b35de9deb97b020833198328c0c22e
numankh <khannuman15@gmail.com>
2024-04-13 16:21:54 -0400

    Updated intent matching script to not use static freqs

--- code/tst/test_intent_matching.py
+++ code/tst/test_intent_matching.py
@@ -10,15 +10,15 @@ from components.intent_matching import get_request_intent
 def test_intent_matching(filename):
     data = pd.read_csv(filename, delimiter=';')
 
+    total_questions = 0
+    total_common_questions = 0
+    total_uncommon_questions = 0
+    total_irrelevant_questions = 0
+
     overall_correct = 0
     common_correct = 0
     uncommon_correct = 0
     none_correct = 0
-
-    total_questions = 14
-    total_common_questions = 6
-    total_uncommon_questions = 4
-    total_irrelevant_questions = 4
 
     for index, row in data.iterrows():
         question = row['Questions']
@@ -26,12 +26,22 @@ def test_intent_matching(filename):
         print(f"Question: {question}")
         print(f"Expected Intent Type: {expected_intent_type}\n")
 
+        # Determine the total number of questions
+        total_questions += 1
+        if expected_intent_type == "COMMON":
+            total_common_questions += 1
+        elif expected_intent_type == "UNCOMMON":
+            total_uncommon_questions += 1
+        elif expected_intent_type == "NONE":
+            total_irrelevant_questions += 1
+
         openai = OpenAiClient()
         response = get_request_intent(question, openai)
 
         actual_intent_type = response[0] if response else "FAILED_INTENT_MATCH"
         print(f"Actual Intent Type: {actual_intent_type}")
 
+        # Check if the intent matching feature works as expected
         if actual_intent_type == expected_intent_type:
             overall_correct += 1
             if actual_intent_type == "COMMON":
@@ -44,11 +54,13 @@ def test_intent_matching(filename):
         time.sleep(2)
         print("===============================")
 
+    # Printing out the accuracy metrics for each intent type
     overall_accuracy = round(overall_correct / total_questions, 2)
     common_accuracy = round(common_correct / total_common_questions, 2)
     uncommon_accuracy = round(uncommon_correct / total_uncommon_questions, 2)
     none_accuracy = round(none_correct / total_irrelevant_questions, 2)
 
+    print("INTENT MATCHING ACCURACY METRICS\n")
     print(f"Overall accuracy: {overall_accuracy}")
     print(f"Common accuracy: {common_accuracy}")
     print(f"Uncommon accuracy: {uncommon_accuracy}")
